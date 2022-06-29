e36326556914fc0b960538b61d245f503485609a
Michael C Koohafkan <michael.koohafkan@gmail.com>
2022-06-29 05:50:02 +0000

    version 3.1-0

--- inst/doc/quickstart.R
+++ inst/doc/quickstart.R
@@ -15,16 +15,20 @@ head(myprofile)
 ## -----------------------------------------------------------------------------
 library(reval)
 library(dplyr)
+library(future)
+library(furrr)
 
 # generate the argument table
-arg.tbl = args_set(n = seq(0.03, 0.06, by = 0.005))
+arg_tbl = args_set(n = seq(0.03, 0.06, by = 0.005))
 
-# define the plan
-plan(sequential) # or could use 'multisession' for parallel processing
+# define the plan (from 'future' package)
+# could also use e.g. 'callr', 'multicore', etc.
+plan(sequential) 
 
 # get output as list-column of argument table
-results = mutate(arg.tbl, 
-  output = future_pmap(arg.tbl, compute_profile,
+# using furrr:future_pmap
+results = mutate(arg_tbl, 
+  output = future_pmap(arg_tbl, compute_profile,
   So = 0.001, Q = 250, y0 = 2.5, Cm = 1.486, g = 32.2,
   B = 100, SS = 0, stepdist = 50, totaldist = 3000)
 )
@@ -32,18 +36,19 @@ results = mutate(arg.tbl,
 ## ----plot-single, echo = FALSE------------------------------------------------
 library(tidyr)
 library(ggplot2)
+
 ggplot(unnest(results, output)) +
   aes(x = x, y = y, color = factor(n)) +
   geom_line() +
   scale_color_viridis_d("Manning's n")
 
 ## -----------------------------------------------------------------------------
-arg.tbl = args_permute(n = seq(0.03, 0.06, by = 0.005),
+arg_tbl = args_permute(n = seq(0.03, 0.06, by = 0.005),
   So = seq(0.001, 0.0015, by = 0.00025),
   SS = seq(0, 6, by = 2)
 )
-results = mutate(arg.tbl, 
-  output = future_pmap(arg.tbl, compute_profile,
+results = mutate(arg_tbl, 
+  output = future_pmap(arg_tbl, compute_profile,
     Q = 250, y0 = 2.5, Cm = 1.486, g = 32.2,
     B = 100, stepdist = 50, totaldist = 3000))
 
